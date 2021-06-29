622478436432aeef86055b59d39d8bde1be0c81a
kimminkwon <kmk130519@gmail.com>
2021-06-29 14:58:43 +0900

    test commit2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to 김민권 Vue.js App" />
+  <HelloWorld msg="Welcome to 김민권2 Vue.js App" />
 </template>
 
 <script>
